86e89e04be61a426b7cfb7b1ad4b5848ee9c39d1
Rizwan Memon <rizwan2000.rm@gmail.com>
2026-02-05 13:38:16 +0530

    Clarify lookback_minutes descriptions
    Remove stray blank/trailing whitespace in GetServiceGraphDescription

--- internal/telemetry/traces/description.go
+++ internal/telemetry/traces/description.go
@@ -5,7 +5,7 @@ const GetExceptionsDescription = `
     Includes the exception type, message, stack trace, service name, trace ID and span attributes.
 
     limit: (Optional) The maximum number of exceptions to return. Defaults to 20.
-    lookback_minutes: (Recommended) Number of minutes to look back from now. Default: 60 minutes.
+    lookback_minutes: (Recommended) Number of minutes to look back from now. Use this for relative time ranges. Default: 60 minutes.
     start_time_iso: (Optional) The start time to get the data from. Leave empty to use lookback_minutes instead.
     end_time_iso: (Optional) The end time to get the data from. Leave empty to default to current time.
     service_name: (Optional) Filter exceptions by service name (e.g. api-service).
@@ -17,7 +17,7 @@ const GetServiceGraphDescription = `
 	Gets the upstream and downstream services for a given span name, along with the throughput for each service.
     Upstream services are the services that are called by the given span name, while downstream services are the services that call the given span.
     The throughput is the number of requests per minute for each service.
-    
+
     span_name: (Required) The name of the span to get dependencies for.
     lookback_minutes: (Recommended) Number of minutes to look back from now. Use this for relative time ranges. Defaults to 60 minutes.
     start_time_iso: (Optional) The start time to get the data from. Leave empty to use lookback_minutes instead.
